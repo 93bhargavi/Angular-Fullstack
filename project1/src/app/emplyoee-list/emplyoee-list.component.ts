9d5b978b23df03e06082b2a38fce81e706038323
93bhargavi <137365939+93bhargavi@users.noreply.github.com>
2023-10-12 19:15:15 +0530

    updated

--- project1/src/app/emplyoee-list/emplyoee-list.component.ts
+++ project1/src/app/emplyoee-list/emplyoee-list.component.ts
@@ -1,6 +1,6 @@
 import { Component , OnInit} from '@angular/core';
-// import { EmpyoeeService } from './empyoee.service';
 import { Route, Router } from '@angular/router';
+import { EmpyoeeService } from '../empyoee.service';
 
 @Component({
   selector: 'app-emplyoee-list',
@@ -8,7 +8,7 @@ import { Route, Router } from '@angular/router';
   styleUrls: ['./emplyoee-list.component.css']
 })
 export class EmplyoeeListComponent implements OnInit {
-   updateEmployee:any;
+  updateEmployee:any;
   deleteEmployee:any;
   employeeDetails:any;
   firstName:any;
@@ -16,13 +16,20 @@ export class EmplyoeeListComponent implements OnInit {
   emailId:any
   id:any;
 
-  employees:EmplyoeeListComponent[] =[];
-
-  constructor(private route:Router) { }
+  constructor(private empyoee:EmpyoeeService, private route:Router) { }
 
   ngOnInit(): void{
+    this.getEmployees();
 
+  }
+  employee_list:any=[];
+  getEmployees(){
+    this.empyoee.getEmployees({firstName:this.firstName,lastName:this.lastName,emailId:this.emailId}).subscribe((res:any) => {
+      this.employee_list= res;
+    });
   }
    
 
 }
+ 
+ 
--- project1/src/app/empyoee.service.ts
+++ project1/src/app/empyoee.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
- 
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class EmpyoeeService {
+  baseurl:any="https://bzadevops.co.in/GMS/api"
+  constructor(private httpclient:HttpClient) {}
+  
+  log_check(data:any)
+    {
+      return this.httpclient.post(this.baseurl+'/login',data)
+    }
 
-  constructor(private : ,private httpclient:HttpClient) {}
+  getEmployees(data:any)
+  {
+    
+  }
 }
